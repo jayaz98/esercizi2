f526b6e19034394adb4fd61ec80aee33ce9da701
jayaz98 <83580687+jayaz98@users.noreply.github.com>
2021-06-15 13:09:31 +0200

    Update quanti-invitati.cpp
    zczdc

--- quanti-invitati.cpp
+++ quanti-invitati.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 #include<fstream>
 #include<cstring>
+using namespace std;
 
 int main(){
+	ifstream in("input.txt");
+	ofstream out("output.txt");
+	
+	
+	
+	for(int i=0; i<3; i++){
+		char nome[100];
+		int n=0;
+		while(nome[n] != '\n'){
+			in >> nome[n];
+			n++;
+		}
+	}
 	return 0;
 }
